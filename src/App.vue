ee62cc0204c76bfd65290439b8f8eeb5d5581d1c
Piccard <piccard@web.de>
2020-05-02 16:43:19 +0200

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col> <img alt="BC logo" src="./assets/bc_logo.svg"/></b-col>
+      </b-row>
+      <b-row>
+        <calc msg="Welcome to Your Vue.js App" />
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Calc from "./components/Calc.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Calc
   }
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+  // margin-top: 60px;
 }
 </style>
---
+++ src/components/Calc.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <!-- quantity -->
+    <b-row class="my-4">
+      <b-col sm="3">
+        <label for="quantity">Anzahl:</label>
+      </b-col>
+      <b-col sm="9">
+        <b-form-input
+          id="quantity"
+          type="text"
+          v-model="quantity"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+
+    <!-- quotation -->
+    <b-row class="my-4">
+      <b-col sm="3">
+        <label for="quotation">Kurs/BC:</label>
+      </b-col>
+      <b-col sm="9">
+        <b-form-input
+          id="quotation"
+          type="text"
+          v-model="quotation"
+        ></b-form-input>
+      </b-col>
+    </b-row>
+
+    <div>
+      <b-alert show>Du erhältst: {{ quantityReal }}</b-alert>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calc",
+  props: {},
+  data() {
+    return {
+      quantity: undefined,
+      quotation: undefined
+    };
+  },
+  computed: {
+    quantityReal() {
+      return parseFloat(this.quantity) * 0.99;
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss"></style>
